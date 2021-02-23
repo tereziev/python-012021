50388a42c4f30d9074f1da099c01c5c468f3d793
tereziev <terezie.vitkova@gmail.com>
2021-02-23 08:52:00 +0100

    zkouška nahrání domácího úkolu na gh

---
+++ domaci_ukoly/01/priklad02.py
@@ -0,0 +1,22 @@
+sklad = {
+  "1N4148": 250,
+  "BAV21": 54,
+  "KC147": 147,
+  "2N7002": 97,
+  "BC547C": 10
+}
+
+kodSoucastky = input("Zadejte kód součástky: ")
+pocestKusu = int(input("Zadejte požadované množství: "))
+
+if kodSoucastky in sklad:
+    soucastka = int(sklad[kodSoucastky])
+    if soucastka >= pocestKusu:
+        print("Poptávku lze uspokojit v plné výši.")
+        sklad[kodSoucastky] = soucastka - pocestKusu
+    else:
+        print(f"Součástka je skladem jen ve výši {soucastka} kusů.")
+        sklad.pop(kodSoucastky)
+else:
+    print(f"Součáskta {kodSoucastky} není skladem.")
+
