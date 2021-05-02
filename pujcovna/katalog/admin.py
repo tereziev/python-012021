d948d39568653b6a800c872efa9014db8855d45e
tereziev <79190776+tereziev@users.noreply.github.com>
2021-05-02 17:03:23 +0200

    Domácí úkoly - sada 9: autopůjčovna

--- pujcovna/katalog/admin.py
+++ pujcovna/katalog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from . import models
+admin.site.register(models.Auto)
+admin.site.register(models.Zakaznik)
+admin.site.register(models.Vypujcka)
--- pujcovna/katalog/models.py
+++ pujcovna/katalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Auto(models.Model):
+    registracni_znacka = models.CharField(max_length=10)
+    znacka_typ = models.CharField(max_length=100)
+    ujeto_km = models.IntegerField()
+    technicka = models.DateField()
+
+class Zakaznik(models.Model):
+    jmeno_prijmeni = models.CharField(max_length=100)
+    cislo_ridicaku = models.IntegerField()
+    datum_narozeni = models.DateField()
+
+class Vypujcka(models.Model):
+    datum_a_cas_zacatku_vypujcky = models.DateTimeField()
+    datum_a_cas_konce_vypujcky  = models.DateTimeField()
+    cena_vypujcky = models.IntegerField()
